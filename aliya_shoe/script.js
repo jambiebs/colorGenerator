6f1a1a5dd64df20d83d5928150dd2ccd19922dd4
Jam_Al <jambiebs@gmail.com>
2022-09-10 14:28:00 +0530

    new day new lession

---
+++ aliya_shoe/script.js
@@ -0,0 +1,12 @@
+//This is my first Javascript code
+console.log('hello world');
+ 
+let person = 'Jamal';
+console.log(person);
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a nubmer (1person)
+// Sannot contain a space or hyhen (-)
+// Are case-sensitive
+
+let firstName = 'Jamal'
