f3ef1cb60c621c316e902fe3944ce867b813fb2d
Jam_Al <jambiebs@gmail.com>
2023-01-14 20:35:10 +0530

    random color project added

---
+++ color-generator/script.js
@@ -0,0 +1,15 @@
+const color = document.querySelector("h1");
+const btn = document.querySelector(".btn");
+const background = document.querySelector("body");
+function randomColorGeneretor() {
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255)
+    const blue = Math.floor(Math.random() * 255)
+    const randomColor = `RGB(${red},${green},${blue})`
+    return randomColor;
+}
+btn.addEventListener("click", () => {
+    const randomColor = randomColorGeneretor();
+    background.style.backgroundColor = randomColor;
+    color.innerHTML = randomColor;
+})
